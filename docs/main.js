bfaf35062d46fd4f2612933a583cf72f785cb6c5
Kendall Weihe <kendallweihe@gmail.com>
2017-12-02 09:44:52 -0500

    threw bootstrap work into dir and started from ground up -- raw JS + CSS styling

--- docs/main.js
+++ docs/main.js
@@ -1,129 +1,61 @@
 
-// TODO:
-//   - get position of right most side of last image
-//     - store in variable
-//   - on load, start on the left? center?
-//   - get a scroll width
-//     - if the scroll width <0 or >right most, calculate difference
 
-function load_scrollers() {
+function style_padding() {
+  // var doc_width = document.body.clientWidth;
+  var doc_height = document.body.clientHeight;
+  // console.log("doc_width: " + doc_width);
+  console.log("doc_height: " + doc_height);
 
-  var total_width = -$('.list').css('right');
-  console.log(total_width);
-
-  var scroll_width = function() {
-    var left_position = left_position();
-    console.log(left_position);
-
-    var standard_scroll_width = (total_width / 4);
-    console.log(standard_scroll_width);
-
-    if (left_position + standard_scroll_width) {
-      return (total_width - left_position);
-    }
-    else {
-      return standard_scroll_width;
-    }
+  var row_pad_height = (doc_height * 0.05);
+  console.log("row_pad_height: " + row_pad_height);
+  var pad_rows = document.getElementsByClassName('row-pad');
+  for (var i=0; i<pad_rows.length; i++) {
+    pad_rows[i].style.height = row_pad_height.toString() + "px";
+    console.log("pad div: " + pad_rows[i]);
   }
-
-  var left_position = function() {
-    return -$('.list').position().left;
-  }
-
-  $('.scroller-right').click(function() {
-    var scroll_amount = scroll_width();
-    $('.scroller-left').fadeIn('slow');
-    $('.list').animate({left:"-="+ scroll_amount +"px"},'slow',function(){ });
-  });
-
-  $('.scroller-right').click(function() {
-    var scroll_amount = scroll_width();
-    $('.scroller-left').fadeIn('slow');
-    $('.list').animate({left:"-="+ scroll_amount +"px"},'slow',function(){ });
-  });
-
-  var adjust = function(){
-    if (($('.wrapper').outerWidth()) < total_width) {
-      $('.scroller-right').show();
-    }
-    else {
-      $('.scroller-right').hide();
-    }
-
-    if (left_position()<0) {
-      $('.scroller-left').show();
-    }
-    else {
-      $('.item').animate({left:"+="+left_position()+"px"},'slow');
-    	$('.scroller-left').hide();
-    }
-  }
-
-  adjust();
-
-  $(window).on('resize',function(e){
-    	adjust();
-  });
-
-  // var widthOfList = function(){
-  //   var itemsWidth = 0;
-  //   $('.list li').each(function(){
-  //     var itemWidth = $(this).outerWidth();
-  //     itemsWidth+=itemWidth;
-  //   });
-  //   return itemsWidth;
-  // };
-  //
-  // var scroll_width = function(){
-  //   return ((($('.wrapper').outerWidth())+widthOfList()) / 4);
-  // };
-  //
-  // var getLeftPosi = function(){
-  //   return $('.list').position().left;
-  // };
-  //
-  // var reAdjust = function(){
-  //   if (($('.wrapper').outerWidth()) < widthOfList()) {
-  //     $('.scroller-right').show();
-  //   }
-  //   else {
-  //     $('.scroller-right').hide();
-  //   }
-  //
-  //   if (getLeftPosi()<0) {
-  //     $('.scroller-left').show();
-  //   }
-  //   else {
-  //     $('.item').animate({left:"+="+getLeftPosi()+"px"},'slow');
-  //   	$('.scroller-left').hide();
-  //   }
-  // }
-  //
-  // reAdjust();
-  //
-  // $(window).on('resize',function(e){
-  //   	reAdjust();
-  // });
-  //
-  // $('.scroller-right').click(function() {
-  //   console.log(getLeftPosi());
-  //   console.log(scroll_width());
-  //   console.log(widthOfList());
-  //
-  //   if ((-getLeftPosi() + scroll_width()) > widthOfList()) {
-  //     scroll_amount = widthOfList() - (-getLeftPosi());
-  //   }
-  //   else {
-  //     scroll_amount = scroll_width();
-  //   }
-  //
-  //   $('.scroller-left').fadeIn('slow');
-  //   $('.list').animate({left:"-="+ scroll_amount +"px"},'slow',function(){ });
-  // });
-  //
-  // $('.scroller-left').click(function() {
-  // 	$('.scroller-right').fadeIn('slow');
-  // 	// $('.scroller-left').fadeOut('slow');
-  //     $('.list').animate({left:"+="+scroll_width()+"px"},'slow',function(){ });
-  // });
 };
+
+function style_navbar() {
+  var doc_width = document.body.clientWidth;
+  var doc_height = document.body.clientHeight;
+  console.log("doc_width: " + doc_width);
+  console.log("doc_height: " + doc_height);
+
+  var nav = document.getElementsByClassName("nav")[0];
+  console.log("nav: " + nav);
+  var nav_height = (doc_height * 0.10);
+  nav.style.height = nav_height.toString() + "px";
+
+  var location_y = (doc_height * 0.10);
+  var side_padding = (doc_width * 0.40);
+  var link_width = ((doc_width * 0.60) / 5);
+  var nav_links = document.getElementsByClassName("nav-link");
+  console.log("location_y: " + location_y);
+  console.log("side_padding: " + side_padding);
+  console.log("link_width: " + link_width);
+  console.log("nav_links: " + nav_links);
+  console.log("nav_links.length: " + nav_links.length);
+  for (var i=0; i<nav_links.length; i++) {
+    nav_links[i].style.width = link_width.toString() + "px";
+    console.log(nav_links[i].style.width);
+
+    var location_x = ((side_padding / 2) + (i * link_width));
+    console.log("location_x: " + location_x);
+    console.log("location_y: " + location_y);
+
+    nav_links[i].style.left = location_x.toString() + "px";
+    nav_links[i].style.top = location_y.toString() + "px";
+    console.log(nav_links[i].style.left);
+    console.log(nav_links[i].style.top);
+  }
+};
+
+function stylize() {
+
+  style_padding();
+  style_navbar();
+
+};
+
+// TODO:
+//   - if resize: then stylize again
