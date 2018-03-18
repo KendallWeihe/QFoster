341ef38e3d1dce1d3851b719854667441764af35
Kendall Weihe <kendallweihe@gmail.com>
2018-03-18 18:16:14 -0400

    working on API Gateway authentication

--- static/public/js/edit.js
+++ static/public/js/edit.js
@@ -3,4 +3,28 @@
 function authenticate()
 {
     console.log("here!");
+
+    var req = new XMLHttpRequest();
+
+    var endpoint = "https://ygvhbipn39.execute-api.us-east-1.amazonaws.com/prod";
+    var data = {
+        "Bearer": "k6t62V^4Kq!3"
+    };
+
+    // $.post(endpoint, data, function(result){
+    //     console.log(result);
+    // });
+
+    $.ajax({
+        url: endpoint,
+        headers: {
+            "Content-Type":"application/json"
+        },
+        method: 'POST',
+        dataType: 'json',
+        data: data,
+        success: function (response){
+            console.log('succes: ' + response);
+        }
+    });
 }
