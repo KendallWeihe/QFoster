1904d77a13388c952d1a552cb829d6d6c4c42ec4
Kendall Weihe <kendallweihe@gmail.com>
2018-03-20 19:54:30 -0400

    main landing page statically there, uploading images to S3

--- static/public/js/edit.js
+++ static/public/js/edit.js
@@ -4,27 +4,40 @@ function authenticate()
 {
     console.log("here!");
 
-    var req = new XMLHttpRequest();
+    // var req = new XMLHttpRequest();
 
-    var endpoint = "https://ygvhbipn39.execute-api.us-east-1.amazonaws.com/prod";
-    var data = {
-        "Bearer": "k6t62V^4Kq!3"
-    };
+    // var endpoint = "https://ygvhbipn39.execute-api.us-east-1.amazonaws.com/prod";
+    // var data = {
+    //     "Bearer": "k6t62V^4Kq!3"
+    // };
 
-    // $.post(endpoint, data, function(result){
-    //     console.log(result);
+    // // $.post(endpoint, data, function(result){
+    // //     console.log(result);
+    // // });
+
+    // $.ajax({
+    //     url: endpoint,
+    //     headers: {
+    //         "Content-Type":"application/json"
+    //     },
+    //     method: 'POST',
+    //     dataType: 'json',
+    //     data: data,
+    //     success: function (response){
+    //         console.log('succes: ' + response);
+    //     }
     // });
 
-    $.ajax({
-        url: endpoint,
-        headers: {
-            "Content-Type":"application/json"
-        },
-        method: 'POST',
-        dataType: 'json',
-        data: data,
-        success: function (response){
-            console.log('succes: ' + response);
-        }
-    });
+    var authenticated = true;
+    if (authenticated)
+    {
+        TODO: 
+            - hide the auth-container 
+            - show the sortable list
+    }    
+}
+
+function get_images(callback)
+{
+
 }
--- static/public/js/main.js
+++ static/public/js/main.js
@@ -41,7 +41,86 @@ document.getElementById("italy").addEventListener("click", function() {
 
 function load_main_img(current_album) {
   console.log(current_album);
+  console.log("getting json...");
+  $.getJSON("https://s3.amazonaws.com/qfoster/config.json", function(data){
+    var main_slide = document.getElementsByClassName("main-slide")[0];
+    var endpoint = "https://s3.amazonaws.com/qfoster";
+    var albums = data.albums;
+    var album = albums[current_album];
+    for (var i = 0; i < album.length; i++)
+    {
+      // var tick = function(i) {
+      //   return function() {
+          console.log(endpoint);
+          var photo = album[i];
+          console.log(photo);
+          var src = endpoint + "/" + current_album + "/" + photo;
+          console.log(src);
+          
+          var div = document.createElement("div");
+          var img = document.createElement("img");
+          img.src = src;
+          img.style.height = "100%";
+          img.style.objectFit = "contain";
+          img.className = "main-slide-img"
+          div.appendChild(img);
+          div.className = "main-image";
+          div.style.height = "100%";
+          div.style.visibility = "hidden";
+          // HIDE VISIBILITY
+          main_slide.appendChild(div);
+      //   }
+      // };
+      // setTimeout(tick(i), 2000 * i);
+      // console.log("next");
+      // IF I = LENGTH-1: FUNCTION TO STYLIZE SLIDE AND UNHIDE VISIBILITY 
+      if (i == (album.length-1))
+      {
+        console.log("here");
+        slide_stylize();
+      }
+    }
+  });
 };
+
+function slide_stylize()
+{
+  console.log("main-slide...");
+  $('.main-slide').slick({
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 1,
+    infinite: true,
+    accessibility: true,
+    // arrows: false,
+    swipeToSlide: true,
+    cssEase: 'linear',
+    fade: true,
+    dots: true,
+    mobileFirst: true,
+    touchThreshold: 15
+  });
+
+  var width = document.getElementsByClassName("main-slide")[0].clientWidth;
+  $(document).find('.slick-list').attr('tabindex', 0).focus();
+  var num_dots = document.getElementsByClassName("slick-dots")[0].getElementsByTagName("li").length;
+  var dots_spacing = ((width * 0.9) / num_dots) / 4;
+  console.log(dots_spacing);
+  var ul = document.getElementsByClassName("slick-dots")[0];
+  var items = ul.getElementsByTagName("li");
+  console.log(items);
+  for (var i = 0; i < items.length; ++i) {
+    items[i].style.margin = "0 " + dots_spacing.toString() + "px";
+    console.log(items[i]);
+  }
+
+  var imgs = document.getElementsByClassName("main-image");
+  console.log(imgs);
+  for (var i = 0; i < imgs.length; i++)
+  {
+    imgs[i].style.visibility = "visible";
+  }
+}
 
 function stylize(){
   var width = document.body.clientWidth;
